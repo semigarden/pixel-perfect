3dffebe3037c13841adea0219ec34028a8df476b
semigarden <semigardenn@gmail.com>
2025-08-10 23:55:27 +0400

    update config

--- src/interface.js
+++ src/interface.js
@@ -17,8 +17,7 @@ const { h } = require('./vdom');
 // }
 
 const Interface = () => {
-  // Example interface composed of a root panel with nested text
-  return Panel({ x: 2, y: 1, width: 40, height: 8, title: 'Pixel Perfect' }, [
+  return Panel({ x: 0, y: 2, width: 40, height: 8, title: 'Pixel Perfect' }, [
     h('text', { x: 1, y: 1, text: 'Hello Virtual DOM' }),
     h('text', { x: 1, y: 2, text: 'Press Q to quit' })
   ]);
